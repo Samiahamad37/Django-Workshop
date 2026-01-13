0c6f935cd3b3eecf28f2d261aff9bcff7af955ef
Samia <samia@gmail.com>
2026-01-13 21:19:24 +0300

    urls for different views

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns =[
-    path('students/', student_ListCreate)
+    # url for @api_view
+    path('students/', student_ListCreate()),
+
+    # url for APIView
+    path('students/', StudentView.as_view()),
+
+    # url for Generic view
+    path('students/', StudentListCreate.as_view()),
+
+    # url for mixin view
+    path('students/', StudenMixinView.as_view()),
+
+    # url for Viewset
+    router = DefaultRouter()
+    router.register('students', StudentViewSet)
+    urlpatterns=router.urls
 ]
