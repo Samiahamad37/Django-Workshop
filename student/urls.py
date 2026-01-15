e81833834e0646bc1871738370fba26a9cb8a074
Samia <hamadsamia37@gmail.com>
2026-01-15 16:54:12 +0300

    solving issues

--- student/urls.py
+++ student/urls.py
@@ -1,21 +1,21 @@
-# from django.urls import path
-# from .views import (
-#     student_ListCreate,
-#     StudentView,
-#     StudentListCreate,
-#     StudenMixinView
-# )
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import StudentViewSet
+from django.urls import path
+from .views import (
+    student_ListCreate,
+    StudentView,
+    StudentListCreate,
+    StudenMixinView
+)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import StudentViewSet
 
-# router = DefaultRouter()
-# router.register(r'students', StudentViewSet, basename='student')
+router = DefaultRouter()
+router.register(r'students', StudentViewSet, basename='student')
 
-# urlpatterns = [
-#     path('students/fbv/', student_ListCreate),
-#     path('students/apiview/', StudentView.as_view()),
-#     path('students/generic/', StudentListCreate.as_view()),
-#     path('students/mixin/', StudenMixinView.as_view()),
-#     path('', include(router.urls)),
-# ]
+urlpatterns = [
+    path('students/fbv/', student_ListCreate),
+    path('students/apiview/', StudentView.as_view()),
+    path('students/generic/', StudentListCreate.as_view()),
+    path('students/mixin/', StudenMixinView.as_view()),
+    path('', include(router.urls)),
+]
