286263257281bff8c230caec0314f6abb181fcaf
Samia <hamadsamia37@gmail.com>
2026-01-15 16:36:10 +0300

    functional view

--- student/views.py
+++ student/views.py
@@ -30,47 +30,47 @@ def student_ListCreate(request):
 
 
 
-# 2. CLASS BASED
-#    i. APIVIEW
+# # 2. CLASS BASED
+# #    i. APIVIEW
 
-class StudentView(APIView):
-    def post(self,request):
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+# class StudentView(APIView):
+#     def post(self,request):
+#         serializer = StudentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
 
-    def get(self, request):
-        student= Student.object.all()
-        serializer = StudentSerializer(student, many=True)
-        return Response
+#     def get(self, request):
+#         student= Student.object.all()
+#         serializer = StudentSerializer(student, many=True)
+#         return Response
     
 
-# ii. Generic view
-class StudentListCreate(ListCreateAPIView):
-    queryset = Student.object.all()
-    serializer_class=StudentSerializer
+# # ii. Generic view
+# class StudentListCreate(ListCreateAPIView):
+#     queryset = Student.object.all()
+#     serializer_class=StudentSerializer
 
 
 
-# iii. mixin view
-class StudenMixinView(mixins.ListModelMixin,
-                     mixins.CreateModelMixin,
-                     generics.GenericAPIView):
-    queryset = Student.object.all()
-    serializer_class=StudentSerializer
+# # iii. mixin view
+# class StudenMixinView(mixins.ListModelMixin,
+#                      mixins.CreateModelMixin,
+#                      generics.GenericAPIView):
+#     queryset = Student.object.all()
+#     serializer_class=StudentSerializer
 
-    def get(self, request):
-        return self.list(request)
+#     def get(self, request):
+#         return self.list(request)
     
-    def post(self, request):
-        return self.create(request) 
+#     def post(self, request):
+#         return self.create(request) 
     
 
 
-# iv. Viewset
-class StudentViewSet(ModelViewSet):
-    queryset = Student.object.all()
-    serializer_class=StudentSerializer
+# # iv. Viewset
+# class StudentViewSet(ModelViewSet):
+#     queryset = Student.object.all()
+#     serializer_class=StudentSerializer
