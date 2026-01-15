910f04d5a829513210cade2580a8fd508ac0e449
Samia <hamadsamia37@gmail.com>
2026-01-15 09:38:05 +0300

    adding models

--- student/models.py
+++ student/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
+
+class Subject(models.Model):
+    student = models.ForeignKey(Student, related_name='subjects', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
