72a7ca459d6c0250924cfce7f2749f6ba7541160
Samia <hamadsamia37@gmail.com>
2026-01-17 08:31:16 +0300

    subject serializer

--- student/serializer.py
+++ student/serializer.py
@@ -5,3 +5,9 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta :
         model = Student
         fields = '__all__'
+
+
+
+class SubjectSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    marks = serializers.IntegerField()
