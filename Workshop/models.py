143042f853e8d28765fe699df9fd2c6ec1eed8b8
Samia <hamadsamia37@gmail.com>
2026-01-23 20:52:07 +0300

    comment model

--- Workshop/models.py
+++ Workshop/models.py
@@ -26,3 +26,11 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
+
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+    def __str__(self):
+        return self.title   
+
